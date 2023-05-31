730664efc494ea80bc16dceabef006051ddf1df9
Paloma Baker <paloma.d.baker@gmail.com>
2023-05-31 11:49:53 -0700

    adding handleMenuOption function

--- mainmenu.js
+++ mainmenu.js
@@ -8,3 +8,109 @@ const connection = mysql.createConnection({
   password: 'ilovemyfamily',
   database: 'company_info',
 });
+
+function handleMenuOption(option) {
+    switch (option) {
+      case 'View all departments':
+        getAllDepartments()
+          .then((departments) => {
+            console.table(departments);
+          })
+          .catch((error) => {
+            console.error('Error retrieving departments:', error);
+          })
+          .finally(() => {
+            showMainMenu();
+          });
+        break;
+      case 'View all roles':
+        getAllRoles()
+          .then((roles) => {
+            // Format and display the roles
+            console.log('Job Title\tRole ID\tDepartment\tSalary');
+            roles.forEach((role) => {
+              console.log(
+                `${role.title}\t${role.role_id}\t${role.department}\t${role.salary}`
+              );
+            });
+          })
+          .catch((error) => {
+            console.error('Error retrieving roles:', error);
+          })
+          .finally(() => {
+            showMainMenu();
+          });
+        break;
+      case 'View all employees':
+        getAllEmployees()
+          .then((employees) => {
+            // Format and display the employees
+            console.log(
+              'Employee ID\tFirst Name\tLast Name\tJob Title\tDepartment\tSalary\tManager'
+            );
+            employees.forEach((employee) => {
+              console.log(
+                `${employee.employee_id}\t${employee.first_name}\t${employee.last_name}\t${employee.job_title}\t${employee.department}\t${employee.salary}\t${employee.manager}`
+              );
+            });
+          })
+          .catch((error) => {
+            console.error('Error retrieving employees:', error);
+          })
+          .finally(() => {
+            showMainMenu();
+          });
+        break;
+      case 'Add a department':
+        addDepartment()
+          .then(() => {
+            showMainMenu();
+          })
+          .catch((error) => {
+            console.error('Error adding department:', error);
+            showMainMenu();
+          });
+        break;
+      case 'Add a role':
+        addRole()
+          .then(() => {
+            showMainMenu();
+          })
+          .catch((error) => {
+            console.error('Error adding role:', error);
+            showMainMenu();
+          });
+        break;
+      case 'Add an employee':
+        addEmployee()
+          .then(() => {
+            showMainMenu();
+          })
+          .catch((error) => {
+            console.error('Error adding employee:', error);
+            showMainMenu();
+          });
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole()
+          .then(() => {
+            showMainMenu();
+          })
+          .catch((error) => {
+            console.error('Error updating employee role:', error);
+            showMainMenu();
+          });
+        break;
+      case 'Exit':
+        connection.end((err) => {
+          if (err) {
+            console.error('Error closing the database connection:', err);
+          } else {
+            console.log('Database connection closed.');
+          }
+          process.exit(); // Terminate the application
+        });
+        break;
+    }
+  }
+  
